e7e81733d15c79ad831a153d178d706714b8ed1b
Josue Kouka <josuebrunel@gmail.com>
2023-08-08 06:58:44 -0700

    image: add image segmentation endpoint (fix #20)

--- app/image/__init__.py
+++ app/image/__init__.py
@@ -1,7 +1,7 @@
 import base64
 from io import BytesIO
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 from fastapi.responses import JSONResponse
 from PIL import Image
 from pydantic import BaseModel
@@ -34,4 +34,23 @@ async def detect(payload: Payload):
     buf = BytesIO(base64.b64decode(payload.b64content))
     img = Image.open(buf)
     result = await processor.detect(img)
+    return ApiResponseList(data=result)
+
+
+@app.post("/segmenter", response_model=ApiResponseList)
+async def segmenter(image: UploadFile):
+    if not image:
+        return ApiResponse(error="no-file-sent")
+    img = Image.open(image.file)
+    img_format = img.format
+    segments = await processor.segment(img)
+    result = []
+    for segment in segments:
+        buf = BytesIO()
+        pil_img = segment["mask"]
+        pil_img.save(buf, format=img_format)
+        result.append({
+            "label": segment["label"],
+            "img": base64.b64encode(buf.getvalue())
+        })
     return ApiResponseList(data=result)
--- app/image/processor.py
+++ app/image/processor.py
@@ -10,3 +10,9 @@ async def classify(img):
 async def detect(img):
     pipe = pipeline("object-detection", model="facebook/detr-resnet-50")
     return pipe(img)
+
+
+async def segment(img):
+    pipe = pipeline("image-segmentation",
+                    model="nvidia/segformer-b0-finetuned-ade-512-512")
+    return pipe(img)
